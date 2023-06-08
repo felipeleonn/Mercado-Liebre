c75b5cba70674c7ec05b47df3c1a80b847041e41
felipeleonn <felipeleon98@outlook.comgit config --global user.email felipeleon98@outlook.com>
2023-06-07 21:45:06 -0300

    .

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const mainRouter = require('./routes/mainRouter');
 
-
+// Colocar ambas líneas para que funcione EJS
+app.set( 'views', path.join(__dirname, 'views') );
+app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
 app.listen( process.env.PORT || 3000, () =>  console.log('Servidor local activado en 3000') );
 
-app.use(mainRouter)
+app.use(mainRouter);
 
 
--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -3,13 +3,16 @@ const path = require('path');
 module.exports = {
 
   index: (req, res) => {
-    res.sendFile( path.resolve('./views/home.html') )
+    res.render('home')
   },
   login: (req, res) => {
-    res.sendFile( path.resolve('./views/login.html') )
+    res.render('login')
   },
   register: (req, res) => {
-    res.sendFile( path.resolve('./views/register.html') )
+    res.render('register')
   }
 
 }
+
+// el segundo parámetro que recibe render puede ser un objeto literal el cual se usa dinamicamente en EJS 
+// El metodo send envía texto y el metodo render la vista
